27c12d22ef5622a49bda90cb4c3135a508f915ae
YousefAliMLS <yousm99@gmail.com>
2025-10-20 01:14:07 +0300

    Committing the registration files

---
+++ Project/Controllers/AuthenticationController.cs
@@ -0,0 +1,29 @@
+﻿using Project.Data;
+using Project.DTO;
+using Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Linq;
+
+
+
+namespace Project.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        //injecting the datacontext so we can communicate with the database
+        public AuthenticationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterDTO request)
+        {
+
+        }
+    }
+}
