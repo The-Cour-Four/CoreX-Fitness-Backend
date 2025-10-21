18d4c65f02c9ad98326937f580434b0cb590eaa6
Yousef Ali <yousm99@gmail.com>
2025-10-21 18:10:08 +0300

    Added the full register logic

--- Project/Controllers/AuthenticationController.cs
+++ Project/Controllers/AuthenticationController.cs
@@ -1,4 +1,4 @@
-﻿using Project.Data;
+using Project.Data;
 using Project.DTO;
 using Project.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +9,7 @@ using System.Data.Linq;
 
 namespace Project.Controllers
 {
-    [Route("api/[controller")]
+    [Route("api/[controller]")]
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
@@ -23,7 +23,24 @@ namespace Project.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserRegisterDTO request)
         {
+            if (await _context.Users.AnyAsync(e => e.Email == request.email))
+                return BadRequest("The user alrady exists. ");
 
+            string passwordHashed = BCrypt.Net.BCrypt.HashPassword(request.password);
+
+            var user = new User
+            {
+                Name = request.userName,
+                passwordHashed = passwordHashed,
+                Email = request.email
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("The user has been created successfully. ");
         }
     }
 }
+
+
+
