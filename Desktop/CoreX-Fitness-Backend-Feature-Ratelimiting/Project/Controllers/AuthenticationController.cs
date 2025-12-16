78fc74b112598829f75b1b73f69bb896589130c6
YousefAliMLS <yousm99@gmail.com>
2025-12-16 19:41:30 +0200

    Fixed merge conflicts

---
+++ Desktop/CoreX-Fitness-Backend-Feature-Ratelimiting/Project/Controllers/AuthenticationController.cs
@@ -0,0 +1,190 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Project.Data;
+using Project.DTO;
+using Project.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+        //injecting the datacontext so we can communicate with the database
+        public AuthenticationController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        //public emailfinder(request)
+        //{
+        //    if reqeust.email == User.email{
+        //        return BadRequest();
+        //    else
+        //        {
+        //            return BadRequest(""):
+        //        }
+        //    }
+        //}
+
+        [HttpPost("ApiTesting")]
+        public async Task<IActionResult> APITesting(string x)
+        {
+            if (x is null)
+                return BadRequest("WORKED WITH NULL! ");
+            else
+                return Ok("WORKED WITHOUT NULL! ");
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(UserRegisterDTO request)
+        {
+            if (await _context.Users.AnyAsync(e => e.Email == request.email))
+                return BadRequest("The user alrady exists. ");
+
+            string passwordHashed = BCrypt.Net.BCrypt.HashPassword(request.password);
+
+            var user = new User
+            {
+                Name = request.userName,
+                passwordHashed = passwordHashed,
+                Email = request.email,
+                Weight = request.weight,
+                Height = request.height,
+                Age = request.age
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("The user has been created successfully. ");
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDTO request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(y => y.Email == request.email);
+
+            if (user is null || !BCrypt.Net.BCrypt.Verify(request.password, user.passwordHashed))
+                return BadRequest("The user does not exist, or the password is incorrect. ");
+
+            string token = CreateToken(user);
+
+            return Ok(new { token = token });
+        }
+
+        [HttpPost("forgotPassword")]
+        public async Task<IActionResult> forgotPassword(ForgotPasswordDTO request)
+        {
+
+            if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.newPassword))
+                return BadRequest("Email and new password are required.");
+
+            var user = await _context.Users.FirstOrDefaultAsync(z => z.Email == request.Email);
+            if (user is null) return BadRequest("This email does not exist.");
+
+            user.passwordHashed = BCrypt.Net.BCrypt.HashPassword(request.newPassword);
+
+            await _context.SaveChangesAsync();
+            return Ok("Password has been changed succesfully. ");
+        }
+
+
+        [HttpPost("PasswordChecker")]
+        public async Task<IActionResult> PasswordChecker(PasswordCherckerDTO request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(M => M.Email == request.CurrentEmail);
+
+
+            if (user is null || (!BCrypt.Net.BCrypt.Verify(request.PasswordChecker, user.passwordHashed)))
+                return BadRequest("The user does not exist or the password is incorrect. ");
+
+            else
+                return Ok();
+        }
+
+
+        [HttpPost("updateUserInformation")]
+        public async Task<IActionResult> updateUserInformation(UpdateUserInformationDTO request) 
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.CurrentEmail);
+
+            if (user is null) return BadRequest("This email does not exist. ");
+
+            //might not need for these checkings, as they are handled in PasswordChecker endpoint. 
+            //if(user is null || (!BCrypt.Net.BCrypt.Verify(request.password, user.passwordHashed)))
+
+            //    return BadRequest("Incorrect password, email, or the User does not exist.");
+
+
+            user.Name = request.userName;
+            user.Email = request.email;
+
+
+            if (!(string.IsNullOrEmpty(request.password)))
+            {
+                user.passwordHashed = BCrypt.Net.BCrypt.HashPassword(request.password);
+            }
+
+
+            user.Weight = request.weight;
+            user.Height = request.height;
+            user.Age = request.age;
+            //user.Gender = request.gender;
+            await _context.SaveChangesAsync();
+            return Ok("User information has been updated successfully.");
+
+        }
+
+        //[HttpGet("ChangeUserPassword")]
+        //public async Task<IActionResult> ChangeUserPassword(ForgotPasswordDTO request)
+        //{
+        //    ForgotPasswordDTO(request.newPassword);
+        //}
+
+        private string CreateToken(User user)
+        {
+            //Statements about the user(id, name , etc...)
+            /*
+             *                  دي البيانات الي هتتشفر لما تتحول و تتبعت
+             *                  على شكل 
+             *                     JSON
+             *                     هل التوكين بتتغير لما باجي في اليوم التاسع ولا لأ؟؟؟؟؟؟
+             */
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name)
+            };
+
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDecriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(14),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDecriptor);
+
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
---
+++ Desktop/CoreX-Fitness-Backend-Feature-Ratelimiting/Project/DTO/UserRegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Project.DTO
+{
+    public class UserRegisterDTO
+    {
+        /*Defining the data that the user will send (DTO)*/
+        public string userName { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public float weight { get; set; }
+        public float height { get; set; }
+        public int age { get; set; }
+    }
+}
