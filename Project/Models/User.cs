cb4e720dc3305fcf6106677f4fe7f12047ed87c4
YousefAliMLS <yousm99@gmail.com>
2025-12-16 17:14:13 +0200

    Fixed the branch conflict, and added the final backend version.

---
+++ Project/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+
+namespace Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string passwordHashed { get; set; }
+        public DateTime creationDate { get; set; } = DateTime.Now;
+
+        public float Weight { get; set; }
+        public float Height { get; set; }
+        public int Age { get; set; }
+
+        //public List<String> Gender { get; set; } = new List<String>();
+
+    }
+}
